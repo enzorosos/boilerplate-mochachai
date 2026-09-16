b6ec9862c0b9fd5e51588cdcfa7c836d12781586
Enzo Nicole Rosos <enzo.rosos21@gmail.com>
2026-09-16 15:36:28 +0800

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -97,19 +97,24 @@ suite('Functional Tests with Zombie.js', function () {
       browser.fill('surname', 'Colombo').then(() => {
         browser.pressButton('submit', function () {
           browser.assert.success();
-          browser.assert.status(200);
           browser.assert.text('span#name', 'Cristoforo');
           browser.assert.text('span#surname', 'Colombo');
-          browser.assert.elements('span#dates', 1);
+          browser.assert.element('span#dates', 1);
           done();
         });
       });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', function () {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.element('span#dates', 1);
+          done();
+        });
+      });
     });
   });
 });
